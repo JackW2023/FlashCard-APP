6f8c87976cc600ddd581b0e300100c7f66275233
JW <daxon2012@gmail.com>
2023-11-21 20:36:36 -0800

    Success Deployment

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-const express =require("express")
-const session= require("express-session")
-const exphbs= require("express-handlebars")
-const sequelize= require("./config/connection")
+const express = require("express")
+const session = require("express-session")
+const exphbs = require("express-handlebars")
+const sequelize = require("./config/connection")
 const helmet = require('helmet')
-const routes= require("./controller")
+const routes = require("./controller")
 
 
-const app= express()
+const app = express()
 app.use(helmet())
 
 const PORT = process.env.PORT || 8080
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
-const sess={
-     secret: "Super secret secret",
-    cookie:{},
-    resave:false,
-     saveUninitialized:true,
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
     store: new SequelizeStore({
-       db: sequelize
- })
- }
+        db: sequelize
+    })
+}
 
- app.use(session(sess))
+app.use(session(sess))
 
-const hbs=exphbs.create({})
+const hbs = exphbs.create({})
 app.engine("handlebars", hbs.engine)
 app.set("view engine", "handlebars")
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static("public"))
 
 app.use(routes)
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`App running on port ${PORT}! ...Click on the link: http://localhost:${PORT}`)
     sequelize.sync()
 })
